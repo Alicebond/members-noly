8c20381f4d26e87fa79da2ed793ce4586e0d2597
Bin <45880588+Alicebond@users.noreply.github.com>
2024-02-15 19:38:10 -0330

    Add controllers and routes

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const homeRouter = require("./routes/home");
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const { username, password } = require("./config");
 
 const app = express();
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/home", homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("Home page");
-});
-
-router.get("/sign-up", function (req, res) {
-  console.log("sign-up");
-  res.render("sign-up-form");
+  res.redirect("/home");
 });
 
 module.exports = router;
